b5a3efba81289ac2d3b31a796b22d6cb12730884
Naruto13133 <58816190+Naruto13133@users.noreply.github.com>
2023-12-02 14:11:41 +0530

    Hotfix for signup.java post reqeust is not working hence rebuild it

---
+++ Test/src/main/java/com/atm/contoller/SignUpp.java
@@ -0,0 +1,89 @@
+package com.atm.contoller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import com.atm.config.DBConfig;
+import com.atm.dao.TwoStepAuthDao;
+import com.atm.dao.User;
+import com.atm.pojo.Patient;
+import com.atm.service.TwoStepVerificationService;
+import com.google.zxing.WriterException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SignUpp extends HttpServlet {
+	
+	DBConfig getCon= new DBConfig();
+	User userDao= new User();
+	Patient patient= new Patient();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String firstName = request.getParameter("firstName");
+	    String lastName = request.getParameter("lastName");
+	    String dobStr = request.getParameter("dob"); 
+	    String country = request.getParameter("country");
+	    String genderParam = request.getParameter("gender");
+	    String addressParam = request.getParameter("address");
+	    String cityParam = request.getParameter("city");
+	    String stateParam = request.getParameter("state");
+	    String zipCodeParam = request.getParameter("zipCode");
+	    String emailParam = request.getParameter("email");
+	    String phoneNumberParam = request.getParameter("phoneNumber");
+	    Patient patient = new Patient();
+	    patient.setFirstName(firstName);
+	    patient.setLastName(lastName);
+	    Date dob = null;
+	    try {
+	        LocalDate dobLocal = LocalDate.parse(dobStr); // Use appropriate date parsing based on your date format
+	        dob = Date.valueOf(dobLocal);
+	    } catch (DateTimeParseException e) {
+	        // Handle date parsing exception
+	        e.printStackTrace();
+	    }
+	    patient.setDateOfBirth(dob);
+
+	    // Set other fields using local variables
+	    patient.setGender(genderParam);
+	    patient.setAddress(addressParam);
+	    patient.setCity(cityParam);
+	    patient.setState(stateParam);
+	    patient.setZipCode(zipCodeParam);
+	    patient.setEmail(emailParam);
+	    patient.setPhoneNumber(phoneNumberParam);
+	    patient.setCountry(country);
+	    
+	    
+	   Connection con= getCon.GetMysqlCon();
+	   try {
+		   String secretekey=TwoStepVerificationService.generateSecretKey();
+			patient.setSecretKey(secretekey);
+		int created=userDao.CreateUser(con,patient);
+		
+		TwoStepAuthDao tsaDao = new TwoStepAuthDao();
+		if (created != 1) 
+		    {
+		    	//response.sendRedirect("/");
+		    	throw new NullPointerException();
+		    }
+		    else {
+		    	String secreteUrl = TwoStepVerificationService.getGoogleAuthenticatorBarCode(secretekey, patient.getEmail(), patient.getPhoneNumber());
+		    	TwoStepVerificationService.createQRCode(patient.getFirstName()+" "+patient.getLastName() ,patient.getPhoneNumber(),secreteUrl);
+		    	//response.sendRedirect("");
+			}
+		}catch (SQLException | WriterException | NullPointerException e) {
+			e.printStackTrace();
+			//response.sendRedirect("" );
+	}
+	}
+	
+	
+}
