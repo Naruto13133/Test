6df76b4b2e8eb10f6f7e25ae6dce8e8f4d8e12e0
Naruto13133 <58816190+Naruto13133@users.noreply.github.com>
2023-12-02 17:29:22 +0530

    added the password checking function js

--- Test/src/main/webapp/Resorces/Js/SignUp.js
+++ Test/src/main/webapp/Resorces/Js/SignUp.js
@@ -2,8 +2,35 @@
  * 
  */
 
+	      
+
 document.addEventListener('DOMContentLoaded', () => {
     const selectDrop = document.querySelector('#country');
+	const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const errorSpan = document.getElementById('error');
+    const form = document.getElementById('sign_up_form');
+	
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent form submission
+	console.log(passwordInput);
+	console.log(confirmPasswordInput);
+	console.log(errorSpan);
+	
+
+      const password = passwordInput.value;
+      const confirmPassword = confirmPasswordInput.value;
+
+      if (password !== confirmPassword) {
+        errorSpan.textContent = 'Passwords do not match.';
+      } else if (password.length < 8) {
+        errorSpan.textContent = 'Password should be at least 8 characters long.';
+      } else {
+        errorSpan.textContent = '';
+        alert('Passwords match!');
+        // Here you can perform further actions with the valid passwords
+      }});
+
 
     fetch('https://restcountries.com/v2/all') // Fetching country data from the API
         .then(res => {
@@ -22,4 +49,5 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(err => {
             console.error('Fetch error:', err);
         });
+        
 });
